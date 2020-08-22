6dc48cab5e1f7a84ffd646119d75d6f980bd8145
Grattyelle Teles <grattyelleteles@gmail.com>
2020-08-22 15:29:44 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
 
     def __init__(self, *filhos, nome=None, idade=36):
         self.idade = idade
@@ -21,7 +22,16 @@ if __name__ == '__main__':
         print(filho.nome)
     maria.sobrenome = 'Teles'   # atribuito dinâmico apenas para o objeto maria.
     del maria.filhos            # removendo dinamicamente um atributo
+    bianka.olhos = 1
+    del bianka.olhos
     print(maria.__dict__)       # __dict__ retorna todos os atributos de instância do objeto, inclusive os dinâmicos
     print(bianka.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)         # só dá para acessar assim pq olhos é um atributo de classe
+    print(maria.olhos)          # outra forma de acessar o atributo de classe é por um objeto do tipo Pessoa
+    print(bianka.olhos)         # outra forma de acessar o atributo de classe é por um objeto do tipo Pessoa
+    print(id(Pessoa.olhos), id(maria.olhos), id(bianka.olhos))    # o id do objeto é o mesmo
+
+
 
 
